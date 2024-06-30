fea8e43bc53f795cfb00006f9f935f20d0483cd9
The-best-ikun <3299674348@qq.com>
2024-06-30 09:39:53 +0800

    怎么又访问不到github了

--- src/main/java/com/suep/demo06_25/controller/admin/StudentCrudViewController.java
+++ src/main/java/com/suep/demo06_25/controller/admin/StudentCrudViewController.java
@@ -1,5 +1,39 @@
 package com.suep.demo06_25.controller.admin;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class StudentCrudViewController {
+    @FXML
+    private Label label;
+
+    @FXML
+    private TextField textField1;
+    @FXML
+    private TextField textField2;
+    @FXML
+    private TextField textField3;
+    @FXML
+    private TextField textField4;
+    @FXML
+    private TextField textField5;
+    @FXML
+    private TextField textField6;
+    @FXML
+    private TextField textField7;
+    @FXML
+    private DatePicker datePicker;
+
+
+
+    //    增删改查的四个按钮的方法事件
+    @FXML
+    private void onButton4(){
+
+    }
+
+
 
 }
