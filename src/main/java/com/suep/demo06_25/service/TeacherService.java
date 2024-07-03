c55dd903248995b69c8f2e7f433176853bdee47c
The-best-ikun <3299674348@qq.com>
2024-07-03 10:43:24 +0800

    controller案例

--- src/main/java/com/suep/demo06_25/service/TeacherService.java
+++ src/main/java/com/suep/demo06_25/service/TeacherService.java
@@ -2,6 +2,7 @@ package com.suep.demo06_25.service;
 
 import com.suep.demo06_25.dao.TeacherDAO;
 import com.suep.demo06_25.impl.TeacherDAOImpl;
+import com.suep.demo06_25.pojo.Teacher;
 
 public class TeacherService {
     private TeacherDAO teacherDAO;
@@ -10,8 +11,10 @@ public class TeacherService {
     public TeacherService() {
         teacherDAO = new TeacherDAOImpl();
     }
-    public getTeacher(String id){
-        return teacherDAO.getTeacher( id);
+
+    //   获取老师
+    public Teacher getTeacher(String id){
+        return teacherDAO.getTeacher(id);
 
     }
 }
