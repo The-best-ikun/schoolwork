8729aa6876e7e3fa141d4976ac9e6d480d447f85
fangx <fangxc2017@outlook.com>
2024-07-03 11:01:48 +0800

    增加新的controller

--- src/main/java/com/suep/demo06_25/controller/ForgetController.java
+++ src/main/java/com/suep/demo06_25/controller/ForgetController.java
@@ -1,4 +1,8 @@
 package com.suep.demo06_25.controller;
 
+import javafx.fxml.FXML;
+
 public class ForgetController {
+    @FXML
+    private
 }
--- src/main/java/com/suep/demo06_25/controller/admin/ReviewCourseViewController.java
+++ src/main/java/com/suep/demo06_25/controller/admin/ReviewCourseViewController.java
@@ -1,5 +1,29 @@
 package com.suep.demo06_25.controller.admin;
 
-public class ReviewCourseViewController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 
+public class ReviewCourseViewController {
+    @FXML
+    private Label label1;//显示课程号
+    @FXML
+    private Label label2;//显示申请人工号
+    @FXML
+    private Label label3;//显示课程名
+    @FXML
+    private Label label4;//显示课程位置
+    @FXML
+    private Label label5;//显示课程具体时间
+    @FXML
+    private  Label label6;//显示课程学分
+    @FXML
+    private Label label7;//显示提交申请时间
+    @FXML
+    private Button button1;//确认审核通过按钮
+    @FXML
+    private Button button2;//确认审核不通过按钮
+    @FXML
+    private TableView tableView;
 }
